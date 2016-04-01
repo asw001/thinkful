1a02af81f8ae56a0118407f9adf50422c68749ba
Aaron Williams <variableapple@gmail.com>
2016-04-01 04:04:57 +0000

    initial commit

---
+++ bicycle.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+
+class Bicycle(object):
+    def __init__(self, name, weight, cost):
+        self.name = name
+        self.weight = weight
+        self.cost = cost
+        self.balance = balance
+
+class BikeShop(object):
+    def __init__(self, name, inventory, balance):
+        self.name = name
+        self.inventory
+        self.balance = balance
+
+    def sell(self, markup, bike):
+        self.markup = markup
+        cost = bike.cost
+        markup_cost = cost * (1 + increase / 100)
+        self.balance += markup_cost 
+
+    def show_balance(self):
+        return self.balance
+
+class Customer(object):
+    def __init__(self, name, money):
+        self.name = name
+        self.money = money
+
