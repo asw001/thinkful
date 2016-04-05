7c0620f42e80a37e1d5de46530d31551cbb25bbb
Aaron Williams <variableapple@gmail.com>
2016-04-05 11:50:58 +0000

    refactored main, removed scaffolding, added as functions in classes

--- bicycle.py
+++ bicycle.py
@@ -1,39 +1,76 @@
 #!/usr/bin/python3
 
+import random
 
 class Bicycle(object):
-    def __init__(self, name, weight, cost, purchased):
+    def __init__(self, name, weight, cost):
         self.name = name
         self.weight = weight
         self.cost = cost
-        self.purchased = purchased
+        self.purchased = False
 
 class BikeShop(object):
-    def __init__(self, name, inventory, balance, markup):
+    def __init__(self, name, inventory, balance):
         self.name = name
         self.inventory = inventory
         self.balance = balance
-        self.markup = markup
+        self.markup = 20
 
+    def do_inventory(self, inventory):
+        self.inventory = inventory
+        for bike in inventory:
+            bike.cost = self.markup_price(bike)
+
+    def get_customer_choice(self, customer):
+        budget = customer.money                 
+        customer_choice = []
+        for bike in self.inventory:
+            if bike.cost <= budget and bike.purchased == False:
+                customer_choice.append(bike) 
+                print("{0}, you might be interested in the {1} bike".format(customer.name, bike.name))
+        return customer_choice
+ 
     def markup_price(self, bike):
         markup_cost = bike.cost * (1 + self.markup / 100)
         return markup_cost
         
     def sell(self, bike, customer):
-        price = self.markup_price(bike)
-        self.balance += price
+        self.balance += bike.cost 
         bike.purchased = True
-        customer.money -= price
+        customer.money -= bike.cost
         customer.owns = True
+ 
+    def set_markup(self, markup):
+        self.markup = markup
  
     def show_balance(self):
         return self.balance
 
+    def show_inventory(self):
+        current_inventory = []
+        for bike in self.inventory:
+            if not bike.purchased:
+                current_inventory.append(bike)
+        if current_inventory:
+            print("We have the following bikes in stock")
+            for bike in current_inventory:
+                print("{0} for ${1}".format(bike.name, bike.cost))
+        else:
+            print("We currently have nothing in stock!")
+
 class Customer(object):
-    def __init__(self, name, money, owns):
+    def __init__(self, name, money):
         self.name = name
         self.money = money
-        self.owns = owns 
+        self.owns = False
+
+    def choose_bike(self, bikes):
+        self.choice = random.choice(bikes)
+        return self.choice
+        
+    def set_owns(self, owns):
+        self.owns = owns
 
     def show_balance(self):
         return self.money
+
--- main.py
+++ main.py
@@ -1,33 +1,27 @@
 #!/usr/bin/python3
 
 from bicycle import *
-import random
 
-mountain_bikeSM = Bicycle("Mountain Bike, Small", 40, 300.00, purchased=False)
-mountain_bikeLG = Bicycle("Mountain Bike, Large", 60, 500.00, purchased=False)
-city_bikeSM = Bicycle("City Bike, Small", 30, 499.00, purchased=False)
-city_bikeLG = Bicycle("City Bike, Large", 40, 699.00, purchased=False)
-child_bikeSM = Bicycle("Childrens Bike, Small", 25, 150.00, purchased=False)
-child_bikeLG = Bicycle("Childrens Bike, Large", 30, 200.00, purchased=False)
+mountain_bikeSM = Bicycle("Mountain Bike, Small", 40, 300.00)
+mountain_bikeLG = Bicycle("Mountain Bike, Large", 60, 500.00)
+city_bikeSM = Bicycle("City Bike, Small", 30, 499.00)
+city_bikeLG = Bicycle("City Bike, Large", 40, 599.00)
+child_bikeSM = Bicycle("Childrens Bike, Small", 25, 90.00)
+child_bikeLG = Bicycle("Childrens Bike, Large", 30, 200.00)
 
-customer1 = Customer("Sharon Jones", 1000.00, owns=False)
-customer2 = Customer("Don Draper", 500.00, owns=False)
-customer3 = Customer("Bill Blaskovitch", 600.00, owns=False)
+customer1 = Customer("Sharon Jones", 1000.00)
+customer2 = Customer("Don Draper", 500.00)
+customer3 = Customer("Bill Blaskovitch", 200.00)
 
 customers = [customer1, customer2, customer3]
 
 bikes = [mountain_bikeSM, mountain_bikeLG, city_bikeSM, city_bikeLG, child_bikeSM, child_bikeLG] 
 
-shop1 = BikeShop("Amy's Bikes", bikes, 2000.00, 20)
+shop1 = BikeShop("Amy's Bikes", bikes, 2000.00)
 
+shop1.do_inventory(bikes)
 
 for customer in customers:
-    for bike in shop1.inventory:
-        bike_cost = shop1.markup_price(bike)
-        if bike_cost <= customer.money and bike.purchased == False and customer.owns == False:
-            shop1.sell(bike, customer)
-            print("{0} you bought {1} for $ {2}. You have $ {3} left over".format(customer.name, bike.name, bike_cost, customer.money))
-            print("{0} Profits: {1}".format(shop1.name, shop1.show_balance()))
-    for available_bike in shop1.inventory:
-        if available_bike.purchased == False:
-            print("Bike: {} is available".format(available_bike.name))
+    shop1.get_customer_choice(customer)
+
+shop1.show_inventory()
