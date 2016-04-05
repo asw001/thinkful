9135eb19a87a26b92a86702b1866aa6c56731ddf
Aaron Williams <variableapple@gmail.com>
2016-04-05 13:00:10 +0000

    some improvement. next iteration will have setters and getters (I think), instead of accessing attributes directly

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ mountain_bikeLG = Bicycle("Mountain Bike, Large", 60, 500.00)
 city_bikeSM = Bicycle("City Bike, Small", 30, 499.00)
 city_bikeLG = Bicycle("City Bike, Large", 40, 599.00)
 child_bikeSM = Bicycle("Childrens Bike, Small", 25, 90.00)
-child_bikeLG = Bicycle("Childrens Bike, Large", 30, 200.00)
+child_bikeLG = Bicycle("Childrens Bike, Large", 30, 150.00)
 
 customer1 = Customer("Sharon Jones", 1000.00)
 customer2 = Customer("Don Draper", 500.00)
@@ -21,7 +21,13 @@ shop1 = BikeShop("Amy's Bikes", bikes, 2000.00)
 
 shop1.do_inventory(bikes)
 
-for customer in customers:
-    shop1.get_customer_choice(customer)
-
 shop1.show_inventory()
+
+for customer in customers:
+    choices = shop1.get_customer_choice(customer)
+    choice = customer.choose_bike(choices)
+    shop1.sell(choice, customer)
+    print("{0} purchased {1} for ${2:.2f}. You have ${3:.2f} left in your bike fund".format(customer.name, choice.name, choice.cost, customer.show_balance()))
+    shop1.show_inventory()
+    print("{0} has made ${1:.2f}".format(shop1.name, shop1.show_balance()))
+
